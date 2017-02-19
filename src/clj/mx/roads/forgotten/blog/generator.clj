0192b80c0ff3e15e423bfbfdc4599d626b470e0d
Donagh MacEan <bobber@forgotten.roads.mx>
2017-02-19 00:27:23 -0600

    Added generator implementation.

--- src/clj/mx/roads/forgotten/blog/generator.clj
+++ src/clj/mx/roads/forgotten/blog/generator.clj
@@ -1,6 +1,14 @@
 (ns mx.roads.forgotten.blog.generator
-  (:require [stasis.core :as stasis]))
+  (:require [mx.roads.forgotten.blog.config :as config]
+            [mx.roads.forgotten.blog.web :as web]
+            [stasis.core :as stasis]
+            [taoensso.timbre :as log]))
 
 (defn run
-  [args]
-  :todo)
+  [& args]
+  (let [out-dir (config/get-output-dir)]
+    (log/infof "Generating static content to %s ..." out-dir)
+    (stasis/export-pages
+      web/routes
+      out-dir)
+    (log/info "Static generation complete.")))
