5177a0dff561a0b9796f90ed52d01ad687306e83
Donagh MacEan <bobber@forgotten.roads.mx>
2017-02-19 00:27:10 -0600

    Added routes and app.

--- src/clj/mx/roads/forgotten/blog/web.clj
+++ src/clj/mx/roads/forgotten/blog/web.clj
@@ -1,4 +1,12 @@
-(ns mx.roads.forgotten.blog.web)
+(ns mx.roads.forgotten.blog.web
+  (:require [mx.roads.forgotten.blog.config :as config]
+            [mx.roads.forgotten.blog.web.content.page :as page]
+            [stasis.core :as stasis]))
+
+(def routes
+  {"/index.html" (page/front-page {})})
+
+(def app (stasis/serve-pages routes))
 
 (defn run
   [& args]
