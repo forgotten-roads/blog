aca2656b400282d14fac8ea7eae0aea8b8a77ddc
Donagh MacEan <bobber@forgotten.roads.mx>
2017-08-20 13:29:32 -0500

    Split out useful functions into util ns.
    These will be used by the social ns.

--- src/clj/mx/roads/forgotten/blog/email/delivery.clj
+++ src/clj/mx/roads/forgotten/blog/email/delivery.clj
@@ -1,31 +1,13 @@
 (ns mx.roads.forgotten.blog.email.delivery
   (:require [clojure.java.io :as io]
-            [dragon.content.core :as content]
             [mx.roads.forgotten.blog.email.content :as email-content]
+            [mx.roads.forgotten.blog.util :as util]
             [postal.core :as postal]
             [taoensso.timbre :as log]
             [trifl.fs :as fs]))
 
 (def to "FRMX New Post Announcements <frmx-new-posts@googlegroups.com>")
 (def from "FRMX Blog Updates <blog-updates@forgotten.roads.mx>")
-
-(defn get-post-data
-  [post-file]
-  (log/debugf "Getting content for %s ..." post-file)
-  (-> post-file
-      (io/resource)
-      (.getFile)
-      (io/file)
-      (content/parse)))
-
-(defn get-message-content-file
-  [post-file]
-  (let [parent-dir (fs/parent (io/file post-file))]
-    (->> email-content/new-post-file
-         (format "%s/%s" parent-dir)
-         (io/resource)
-         (.getFile)
-         (io/file))))
 
 (defn make-message
   [to from subject content]
@@ -52,12 +34,8 @@
 
 (defn send-new-post-message
   [post-file]
-  (let [post-data (get-post-data post-file)
-        msg-file (get-message-content-file post-file)
+  (let [post-data (util/get-post-data post-file)
+        msg-file (util/get-message-content-file
+                   post-file email-content/new-post-file)
         msg-content (slurp msg-file)]
     (send-message (:title post-data) msg-content)))
-
-(comment
-  (def posts (blog/process (config/posts-path)))
-  (def file "posts/2017-08/17-202020/content.rfc5322")
-  (def post-data (email-delivery/get-post-data file)))
--- src/clj/mx/roads/forgotten/blog/util.clj
+++ src/clj/mx/roads/forgotten/blog/util.clj
@@ -1,6 +1,29 @@
-(ns mx.roads.forgotten.blog.util)
+(ns mx.roads.forgotten.blog.util
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [dragon.content.core :as content]
+            [taoensso.timbre :as log]
+            [trifl.fs :as fs]))
 
 (defn zip
   [& colls]
   (partition (count colls)
              (apply interleave colls)))
+
+(defn get-post-data
+  [post-file]
+  (log/debugf "Getting content for %s ..." post-file)
+  (-> post-file
+      (io/resource)
+      (.getFile)
+      (io/file)
+      (content/parse)))
+
+(defn get-message-content-file
+  [post-file msg-filename]
+  (let [parent-dir (fs/parent (io/file post-file))]
+    (->> msg-filename
+         (format "%s/%s" parent-dir)
+         (io/resource)
+         (.getFile)
+         (io/file))))
