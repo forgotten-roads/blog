d5c76787b82dc38408ea6fc123be8c9f78cda904
Donagh MacEan <bobber@forgotten.roads.mx>
2017-10-22 19:58:02 -0500

    Removed block code and use the new Dragon blocks.

--- src/clj/mx/roads/forgotten/blog/web/content/data.clj
+++ src/clj/mx/roads/forgotten/blog/web/content/data.clj
@@ -1,6 +1,7 @@
 (ns mx.roads.forgotten.blog.web.content.data
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
+            [dragon.blog.content.block :as block]
             [dragon.blog.core :as blog]
             [dragon.blog.tags :as blog-tags]
             [dragon.config :as config]
@@ -11,62 +12,10 @@
 ;;;   Helper Functions & Data Helpers   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def legal-block-extensions
-  #{".selmer-block"})
-
 (def legal-block-names
-  #{"pre-css"
-    "pre-head-scripts"
-    "post-head-scripts"
-    "head-postpends"
-    "post-post-scripts"
-    "article-body-ads"
-    "article-sidebar-comments-links"})
-
-(defn legal-block-file?
-  [^java.io.File file]
-  (->> legal-block-extensions
-       (map #(string/ends-with? (.getCanonicalPath file) %))
-       (remove false?)
-       (not-empty)))
-
-(defn legal-block-name?
-  [block-name]
-  (contains? legal-block-names block-name))
-
-(defn block-matches?
-  [block-file block-name]
-  (and (legal-block-name? block-name)
-       (string/includes? (str block-file) block-name)))
-
-(defn get-block-name
-  "Given a file object, "
-  [^java.io.File block-file]
-  (->> legal-block-names
-       (filter (partial block-matches? block-file))
-       (first)))
-
-(defn get-block-files
-  [parent-dir]
-  (->> parent-dir
-       (io/file)
-       (.listFiles)
-       (filter legal-block-file?)))
-
-(defn get-block
-  [block-file]
-  (let [block-name (get-block-name block-file)]
-    (if (nil? block-name)
-      []
-      [(keyword block-name) (slurp block-file)])))
-
-(defn get-blocks
-  [post-data]
-  (->> post-data
-       :src-dir
-       (get-block-files)
-       (map get-block)
-       (into {})))
+  (block/legal-block-names
+   #{"article-body-ads"
+     "article-sidebar-comments-links"}))
 
 (defn posts-stats
   [posts]
@@ -197,7 +146,7 @@
       (common posts)
       (assoc-in [:page-data :active] "archives")
       (assoc :post-data post-data
-             :blocks (get-blocks post-data)
+             :blocks (block/get-blocks legal-block-names post-data)
              :tags (blog-tags/unique [post-data]))))
 
 (defn front-page
